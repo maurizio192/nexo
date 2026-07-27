4b7cb322a552bbdcb0c6f3ca4865eb9e3b99db55
Maurizio Agosta <casasmartmauri@gmail.com>
2026-07-27 11:57:25 +0200

    Refactor inicial del modulo Nueva Receta

--- src/pages/LibroRecetas.jsx
+++ src/pages/LibroRecetas.jsx
@@ -9,7 +9,7 @@ export default function LibroRecetas() {
 
   useEffect(() => {
 
-   fetch("http://127.0.0.1:3001/api/recetas/categorias/lista")
+  fetch("http://127.0.0.1:3001/api/recetas/categorias")
       .then(res => res.json())
       .then(data => setCategorias(data))
       .catch(console.error);
@@ -30,57 +30,49 @@ export default function LibroRecetas() {
         📖 Libro de Recetas
       </h1>
 
-  {categorias.map((categoria) => (
+       {categorias.map((categoria) => (
 
-  <div
-    key={categoria.categoria}
-    onClick={() =>
-      navigate(`/categoria/${encodeURIComponent(categoria.categoria)}`)
-    }
-    style={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
-      background: "white",
-      padding: "18px 22px",
-      marginBottom: "12px",
-      borderRadius: "12px",
-      boxShadow: "0 2px 8px rgba(0,0,0,.08)",
-      cursor: "pointer"
-    }}
-  >
+        <div
+          key={categoria.id}
+          onClick={() =>
+            navigate(`/categoria/${encodeURIComponent(categoria.nombre)}`)
+          }
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            background: "white",
+            padding: "18px 22px",
+            marginBottom: "12px",
+            borderRadius: "12px",
+            boxShadow: "0 2px 8px rgba(0,0,0,.08)",
+            cursor: "pointer"
+          }}
+        >
 
-    <h2 style={{ margin: 0 }}>
-      {categoria.categoria}
-    </h2>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "15px"
+            }}
+          >
 
-    <span>
-      {categoria.total} recetas ▶
-    </span>
+            <span style={{ fontSize: "30px" }}>
+              {categoria.icono}
+            </span>
 
-  </div>
+            <h2 style={{ margin: 0 }}>
+              {categoria.nombre}
+            </h2>
 
-))}
+          </div>
 
+          <span>▶</span>
 
-      <div
-        onClick={() => navigate("/recetas/nueva")}
-        style={{
-          marginTop: "35px",
-          background: "#0ea5e9",
-          color: "white",
-          padding: "18px",
-          borderRadius: "12px",
-          textAlign: "center",
-          cursor: "pointer",
-          fontWeight: "bold",
-          fontSize: "18px"
-        }}
-      >
+        </div>
 
-        ➕ Nueva receta
-
-      </div>
+      ))}
 
     </div>
 
